3bfac275b6aa531449f132096a5bbe7b21e20458
Jaymin Dattani <work.jdattani@gmail.com>
2026-09-12 22:39:28 +0530

    Configure CORS for production frontend

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,10 @@ app = FastAPI(title="Sahay - SIH26092")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+   allow_origins=[
+    "https://sahaysetu.vercel.app",
+    "http://localhost:5173",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
